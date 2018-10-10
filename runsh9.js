6434a644ea2ed4c30eeb8217fcc4a7a976f60596
waterslidekid <waterslideparent@gmail.com>
2018-10-09 22:07:00 -0400

    made it more readable - js function

--- runsh9.js
+++ runsh9.js
@@ -3,19 +3,25 @@ Module.print = console.log;
 
 Module.onRuntimeInitialized = function() {
 
-var sh = Module._stringhash9a_create(100000);
-Module.print("pointer sh " + sh);
+ var sh = Module._stringhash9a_create(100000);
+ Module.print("pointer sh " + sh);
 
-let MAXBYTES = 64
+ var MAXBYTES = 64
+ var dataPtr = Module._malloc(MAXBYTES);
 
-let dataPtr = Module._malloc(MAXBYTES);
+ function stringhash9_set(str) {
+   //copy string onto allocated memory buffer
+   let strlen = Module.lengthBytesUTF8(str);
+   strlen = (strlen <= MAXBYTES) ? strlen : MAXBYTES;
+   Module.stringToUTF8(str, dataPtr, MAXBYTES);
 
-let str = "foo"
-let strlen = Module.lengthBytesUTF8(str);
-strlen = (strlen <= MAXBYTES) ? strlen : MAXBYTES;
-Module.stringToUTF8(str, dataPtr, MAXBYTES);
-Module.print("set foo " + Module._stringhash9a_set(sh, dataPtr, strlen));
-Module.print("set foo " + Module._stringhash9a_set(sh, dataPtr, strlen));
-Module.print("set foo " + Module._stringhash9a_set(sh, dataPtr, strlen));
+   //call stringhash with pointer in C-code
+   return Module._stringhash9a_set(sh, dataPtr, strlen);
+ }
+
+ Module.print("result: " + stringhash9_set("hello"));
+ Module.print("result: " + stringhash9_set("hello1"));
+ Module.print("result: " + stringhash9_set("hello"));
+ Module.print("result: " + stringhash9_set("hello1"));
 
 };
